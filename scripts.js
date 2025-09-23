7a899042f7aeb0db32643b35c180ce65c24161b4
ConcertoAudioMalang <perwirateguh1@gmail.com>
2025-09-23 15:18:06 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+// Aksi untuk tombol "Klik Saya!"
 document.getElementById('myButton').addEventListener('click', function() {
     alert('Tombol berhasil diklik! 🎉');
+});
+
+// Aksi untuk tombol Hamburger Menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
