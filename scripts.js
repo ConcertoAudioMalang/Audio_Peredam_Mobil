3e74f1183fc1a3254377120779950866b9414e95
ConcertoAudioMalang <perwirateguh1@gmail.com>
2025-09-23 17:11:48 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,30 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// Aksi untuk Dark/Light Mode
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+// Periksa preferensi tema dari localStorage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.classList.add(currentTheme);
+    if (currentTheme === 'dark-mode') {
+        themeToggleBtn.textContent = '🌙';
+    }
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    // Toggle class 'dark-mode' pada body
+    body.classList.toggle('dark-mode');
+
+    // Ubah ikon tombol
+    if (body.classList.contains('dark-mode')) {
+        themeToggleBtn.textContent = '🌙';
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        themeToggleBtn.textContent = '☀️';
+        localStorage.setItem('theme', 'light-mode');
+    }
+});
