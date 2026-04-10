18df75621b7071503f0f022aebc67e3ae4d7eb59
ConcertoAudioMalang <231805460+ConcertoAudioMalang@users.noreply.github.com>
2026-04-10 16:28:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -268,3 +268,19 @@ document.querySelectorAll('.accordion-header').forEach(header => {
         }
     });
 });
+
+document.querySelectorAll('.accordion-header').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+        const icon = button.querySelector('.accordion-icon');
+
+        button.classList.toggle('active');
+        icon.classList.toggle('rotate-180');
+
+        if (button.classList.contains('active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        } else {
+            accordionContent.style.maxHeight = 0;
+        }
+    });
+});
